6593e06cddc3f3d1aa4791db9cb1d70480168796
auchan <auchan129@foxmail.com>
2019-06-16 18:19:35 +0800

    1.change: 修改文件名：douyu_danmu.py -> danmu_server.py

--- monitor_restart.sh
+++ monitor_restart.sh
@@ -2,10 +2,10 @@
 
 while true
 do
-	ps -ef | grep "python3 douyu_danmu.py 2550505" | grep -v "grep" >> /dev/null
+	ps -ef | grep "python3 danmu_server.py 2550505" | grep -v "grep" >> /dev/null
 if [ "$?" -eq 1 ]
 then
-	python3 douyu_danmu.py 2550505 2>> error.log >>danmu.log  &
+	python3 danmu_server.py 2550505 2>> error.log >>danmu.log  &
 	echo $(TZ='Asia/Shanghai' date "+%Y-%m-%d %H:%M:%S") "process has been restarted"
 else
 	echo "process already started" >> /dev/null
